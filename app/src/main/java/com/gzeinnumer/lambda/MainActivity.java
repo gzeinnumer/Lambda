7eb94e273b6aad5ad6c848be1606dcb737d7909b
Zein <m.fadli.zein.17@gmail.com>
2020-10-15 16:49:37 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/lambda/MainActivity.java
@@ -0,0 +1,26 @@
+package com.gzeinnumer.lambda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.click).setOnClickListener(v -> {
+
+        });
+    }
+}
